447c42573aa8e276ff8ebdd414991888d21de36f
JunedSetiawan <junedhayko502@gmail.com>
2024-01-14 12:59:22 +0700

    update: add footer

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,17 +1,16 @@
 <div class="font-sans text-base-content antialiased">
-    <x-navbar>
-        <div class="flex flex-col justify-between min-h-screen">
-            <main>
-                {{ $slot }}
-            </main>
-
-            <footer class="footer footer-center p-4 bg-base-300 text-base-content">
+    <x-navbar />
+    <div class="flex flex-col justify-between min-h-screen">
+        <main>
+            {{ $slot }}
+        </main>
+        <x-footer />
+        {{-- <footer class="footer footer-center p-4 bg-base-300 text-base-content">
                 <div>
                     <p>Copyright © {{ \Carbon\Carbon::now()->format('Y') }} - All right reserved by
                         {{ env('COPY_RIGHT') }}
                     </p>
                 </div>
-            </footer>
-        </div>
-    </x-navbar>
+            </footer> --}}
+    </div>
 </div>
